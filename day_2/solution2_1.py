5be9d3094d13bdf6f95cd1f07f4548c1b5ed026c
xtooth <jamsen.kim@gmail.com>
2022-12-02 21:26:40 +0200

    simplify solution 2_1

--- day_2/solution2_1.py
+++ day_2/solution2_1.py
@@ -21,17 +21,12 @@ points_dict = {
     'C': 2
 }
 
-
 points_array = [
    # R P S
     [4,1,7],    # rock 
-    [7,4,1],    # paper 
-    [1,7,4]     # scissors 
+    [8,5,2],    # paper 
+    [3,9,6]     # scissors 
 ]
-
-def determine_points(opponents_choise, own_choise):
-    return points_dict[own_choise] + points_array[points_dict[own_choise]][points_dict[opponents_choise]]
-    
 
 file = open('input.txt', 'r')
 lines = file.readlines()
@@ -41,6 +36,6 @@ for line in lines:
     line_as_list = line.strip().split(' ')
     opponents_choise = line_as_list[0]
     own_choise = line_as_list[1]
-    total_points += determine_points(opponents_choise=opponents_choise, own_choise=own_choise)
+    total_points += points_array[points_dict[own_choise]][points_dict[opponents_choise]]
 
 print(total_points)
