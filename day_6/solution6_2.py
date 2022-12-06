93344defb03bc07451efc09d04d7691a1eaf0fa6
xtooth <jamsen.kim@gmail.com>
2022-12-06 10:33:18 +0200

    cleanup

--- day_6/solution6_2.py
+++ day_6/solution6_2.py
@@ -3,6 +3,6 @@ line = file.readlines()[0]
 
 required_len = 14
 for i in range(required_len,len(line)):
-    if(len(set(line[(i-required_len):i])) == required_len):
+    if len(set(line[(i-required_len):i])) == required_len:
         print(i)
-        break;
+        break
