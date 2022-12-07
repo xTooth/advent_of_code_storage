71d45a8804187e8f2a3e62266d0b33b15c46b879
xtooth <jamsen.kim@gmail.com>
2022-12-07 09:43:21 +0200

    Day 7 solution 1

---
+++ day_7/solution7_1.py
@@ -0,0 +1,52 @@
+'''
+{
+    'dir_name': {    
+        'own_size': 'dir_size',
+        'sub_dirs': [dir_names...]
+    }
+}
+'''
+
+current_location = []
+current_location_name = ''
+info_dict = {}
+
+file = open('input.txt', 'r')
+lines = file.readlines()
+
+for line in lines:
+    commands = line.strip().split(' ')
+    if commands[1] == 'cd' and commands[2] != '..':
+        current_location.append(f'_{commands[2]}')
+        current_location_name = ''.join(current_location)
+        if not info_dict.get(current_location_name):
+            info_dict[current_location_name] = {
+                'own_size': 0,
+                'sub_dirs': []
+            }
+    elif commands[0] == 'dir':
+        sub_dir_name = current_location_name + '_' + commands[1]
+        info_dict[current_location_name]['sub_dirs'].append(sub_dir_name)
+    elif commands[0].isnumeric():
+        info_dict[current_location_name]['own_size'] += int(commands[0])
+    else:
+        if commands[1] == 'cd' and commands[2] == '..':
+            current_location.pop()
+            current_location_name = ''.join(current_location)
+
+
+def calculate_value(d: dict):
+    val = d['own_size']
+    if(len(d['sub_dirs']) > 0):
+        for x in d['sub_dirs']:
+            val += calculate_value(info_dict[x])
+    return val
+
+
+sum = 0
+for d in info_dict.values():
+    temp_sum = calculate_value(d)
+    if temp_sum < 100000:
+        sum += temp_sum
+
+print(sum)
