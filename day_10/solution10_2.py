ca24f0b3d9fe040415978157965bb670ea4113a1
xtooth <jamsen.kim@gmail.com>
2022-12-11 17:49:22 +0200

    Some bubblegum to 10_2  to make print more readable.

--- day_10/solution10_2.py
+++ day_10/solution10_2.py
@@ -11,17 +11,18 @@ for line in lines:
 
 screen = [[],[],[],[],[],[]]
 row = 0
+pos = 0
 while len(queue) > 0:
+    pos += 1
     x += queue.pop(0)
-    pos +=1
-    pixel = '#' if pos in range(x-1, x+2) else '.'
+    pixel = '#' if pos in {x-1, x, x+1} else '.'
     screen[row].append(pixel)
-    if(pos == 40):
-        pos = 0;
+    if len(screen[row]) == 40:
         row +=1
+        pos = 0
 
 for x in screen:
-    p = ''
+    p = '..#'
     for y in x:
         p += y
     print(p)
