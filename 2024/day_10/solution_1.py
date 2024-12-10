9bb4a0c9b5356f68868325eae2dc2eb5fb918db5
xtooth <jamsen.kim@gmail.com>
2024-12-10 16:58:01 +0200

    2024: day 10 solutions

---
+++ 2024/day_10/solution_1.py
@@ -0,0 +1,34 @@
+file = open('input.txt', 'r')
+lines = file.readlines()
+
+map = []
+hilltops = []
+for y, line in enumerate(lines):
+    map.append([])
+    for x, char in enumerate(line.strip()):
+        map[y].append(int(char))
+        if int(char) == 9:
+            hilltops.append((y, x))
+
+print(map)
+ans = 0
+
+moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+for hilltop in hilltops:
+    queue = [hilltop]
+    visited = set()
+    while queue:
+        pos = queue.pop(0)
+        if map[pos[0]][pos[1]] == 0 and pos not in visited:
+            ans += 1
+            visited.add(pos)
+        else:
+            for move in moves:
+                new_y = pos[0] + move[0]
+                new_x = pos[1] + move[1]
+                if 0 <= new_y < len(map) and 0 <= new_x < len(map[0]):
+                    if map[pos[0] + move[0]][pos[1] + move[1]] == map[pos[0]][pos[1]] - 1:  # noqa E501
+                        queue.append((pos[0] + move[0], pos[1] + move[1]))
+
+print(ans)
